8553bc39947443d32d9b04e35f0b57cd5d50e829
Mac <mac@kevinChandeiMac.local>
2018-12-27 16:08:40 +0800

    上传项目

---
+++ RHFiltrate/RHFiltrateView.h
@@ -0,0 +1,27 @@
+//
+//  RHFiltrateView.h
+//  MCSchool
+//
+//  Created by 郭人豪 on 2016/12/24.
+//  Copyright © 2016年 Abner_G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol RHFiltrateViewDelegate;
+@interface RHFiltrateView : UIView
+
+@property (nonatomic, weak) id<RHFiltrateViewDelegate> delegate;
+
+- (instancetype)initWithFrame:(CGRect)frame titles:(NSArray<NSString *> *)titles items:(NSArray<NSArray *> *)items;
+
+- (instancetype)initWithTitles:(NSArray<NSString *> *)titles items:(NSArray<NSArray *> *)items;
+//重置
+-(void)clearData;
+@end
+@protocol RHFiltrateViewDelegate <NSObject>
+
+@optional
+- (void)filtrateView:(RHFiltrateView *)filtrateView didSelectAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
